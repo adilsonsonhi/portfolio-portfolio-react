f0126b5b8085281d635642eb69ab19b974cc8cc1
Adilson Sonhi <adilsonsonhi1@gmail.com>
2025-04-09 15:13:56 +0100

    9 April, 2025 - 15:13

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -1,6 +1,7 @@
 import p_img1 from "./images/p_img1.png";
 import p_img2 from "./images/p_img2.png";
 import p_img3 from "./images/p_img3.png";
+import p_img4 from "./images/p_img4.png";
 import service1 from "./images/service-1.png";
 import service2 from "./images/service-2.png";
 
@@ -41,7 +42,7 @@ import mongodb from "./icons/mongodb.svg";
 import mysql from "./icons/mysql.svg";
 import nextjs from "./icons/nextjs.svg";
 import nodejs from "./icons/nodejs.svg";
-import php from "./icons/php.svg";
+// import php from "./icons/php.svg";
 import postman from "./icons/postman.svg";
 import python from "./icons/python.svg";
 import react from "./icons/react.svg";
@@ -55,6 +56,33 @@ import circle_figma from "./icons/circle-figma.svg";
 import circle_react from "./icons/circle-react.svg";
 import circle_nodejs from "./icons/circle-nodejs.svg";
 import circle_mongodb from "./icons/circle-mongodb.svg";
+
+// RESOURCES ICONS
+import codepen from "./icons/codepen.svg";
+import colorhunt from "./icons/colorhunt.svg";
+import css_tricks from "./icons/csstricks.svg";
+import dribbble from "./icons/dribbble.svg";
+import iconify from "./icons/iconify.svg";
+import emmet from "./icons/emmet.svg";
+import mockupplugin from "./icons/mockupplugin.svg";
+import free_formatter from "./icons/freeformatter.svg";
+import google_page_speed from "./icons/google-page-speed.svg";
+import google_analytics from "./icons/googleanalytics.svg";
+import muzli from "./icons/muzli.svg";
+import hostinger from "./icons/hostinger.svg";
+import pexels from "./icons/pexels.svg";
+import pixabay from "./icons/pixabay.svg";
+import prettier from "./icons/prettier.svg";
+import uicolors from "./icons/uicolors.svg";
+import uigradients from "./icons/uigradients.svg";
+import unsplash from "./icons/unsplash.svg";
+import vercel from "./icons/vercel.svg";
+
+// CONTACT ICONS
+import message_circle from "./icons/message-circle.svg";
+import instagram from "./icons/instagram.svg";
+import linkedin from "./icons/linkedin.svg";
+import map_pin from "./icons/map-pin.svg";
 
 export const assets = {
   logo,
@@ -74,7 +102,39 @@ export const assets = {
   service2,
   figma,
   mousecursor,
+  github,
+  // ----
+  message_circle,
+  instagram,
+  linkedin,
+  map_pin,
+  // ----
+  codepen,
+  colorhunt,
+  css_tricks,
+  dribbble,
+  iconify,
+  emmet,
+  mockupplugin,
+  free_formatter,
+  google_page_speed,
+  google_analytics,
+  muzli,
+  hostinger,
+  pexels,
+  pixabay,
+  uicolors,
+  uigradients,
+  unsplash,
+  vercel,
 };
+
+export const socialmedia = [
+  { facebook: "https://wwww.facebook.com/adilsonsonhi" },
+  { instagram: "https://wwww.facebook.com/adilsonsonhi" },
+  { whatsapp: "https://wwww.facebook.com/adilsonsonhi" },
+  { github: "https://wwww.facebook.com/adilsonsonhi" },
+];
 
 export const Services = [
   {
@@ -169,7 +229,7 @@ export const products = [
       "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas, dignissimos! Exercitationem dolores, debitis a mollitia neque eum incidunt consequatur in ratione facilis temporibus aut optio repellendus voluptatibus soluta ea blanditiis.",
     image: [p_img1],
     techs: ["a", "b", "c"],
-    category: "Web Development",
+    category: "Web Dev",
     link: "https://www.adilsonsonhi.com",
   },
   {
@@ -179,7 +239,7 @@ export const products = [
       "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas, dignissimos! Exercitationem dolores, debitis a mollitia neque eum incidunt consequatur in ratione facilis temporibus aut optio repellendus voluptatibus soluta ea blanditiis.",
     image: [p_img2],
     techs: ["a", "b", "c"],
-    category: "Mobile App Desgin",
+    category: "Mobile Design",
     link: "https://www.adilsonsonhi.com",
   },
   {
@@ -189,7 +249,17 @@ export const products = [
       "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas, dignissimos! Exercitationem dolores, debitis a mollitia neque eum incidunt consequatur in ratione facilis temporibus aut optio repellendus voluptatibus soluta ea blanditiis.",
     image: [p_img3],
     techs: ["a", "b", "c"],
-    category: "Mobile App Design",
+    category: "Mobile Design",
+    link: "https://www.adilsonsonhi.com",
+  },
+  {
+    _id: "4",
+    name: "Idealista MarketPlace [Redesign]",
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas, dignissimos! Exercitationem dolores, debitis a mollitia neque eum incidunt consequatur in ratione facilis temporibus aut optio repellendus voluptatibus soluta ea blanditiis.",
+    image: [p_img4],
+    techs: ["a", "b", "c"],
+    category: "Web Design",
     link: "https://www.adilsonsonhi.com",
   },
 ];
@@ -206,7 +276,7 @@ export const skills = [
   { name: "Angular", icon: angular },
   { name: "Node.js", icon: nodejs },
   { name: "Express.js", icon: expressjs },
-  { name: "Php", icon: php },
+  // { name: "Php", icon: php },
   { name: "C#", icon: csharp },
   { name: ".NET", icon: dotNet },
   { name: "Figma", icon: figma },
@@ -221,3 +291,231 @@ export const skills = [
   { name: "Postman", icon: postman },
   { name: "AWS", icon: aws },
 ];
+
+export const Resources = [
+  {
+    _id: "1",
+    category: "Web Dev",
+    icon: codepen,
+    title: "Code Pen",
+    subtitle: "Online Tool",
+    description: "The best place to build, test, and discover front-end code. CodePen is a social development environment for front-end designers and developers.",
+    link: "#",
+  },
+  {
+    _id: "2",
+    category: "Design/Media",
+    icon: colorhunt,
+    title: "Color Hunter",
+    subtitle: "Color palette",
+    description: "Color Palettes for Designers and Artists: Explore the latest hand-picked palettes on Color Hunt, curated for creative inspiration.",
+    link: "#",
+  },
+  {
+    _id: "3",
+    category: "Web Dev",
+    icon: css_tricks,
+    title: "CSS Tricks",
+    subtitle: "Web tutorials",
+    description: "A hub for web developers offering expert tutorials and tools focused on mastering CSS, HTML, and JavaScript.",
+    link: "#",
+  },
+  {
+    _id: "4",
+    category: "Design/Media",
+    icon: dribbble,
+    title: "Dribbble",
+    subtitle: "Design Hub",
+    description: "Dribbble is a community for designers to showcase their work, find inspiration, and connect with others in the creative industry.",
+    link: "#",
+  },
+  {
+    _id: "5",
+    category: "Code Extension",
+    icon: emmet,
+    title: "Emmet Cheat Sheet",
+    subtitle: "VS Code Extension",
+    description: "Emmet is a plugin for many popular text editors which greatly improves HTML & CSS workflow.",
+    link: "#",
+  },
+  {
+    _id: "6",
+    category: "Web Dev",
+    icon: firebase,
+    title: "Firebase",
+    subtitle: "BaaS",
+    description: "Services for building and managing web and mobile applications, including real-time databases, authentication, and hosting.",
+    link: "#",
+  },
+  {
+    _id: "7",
+    category: "Web Dev",
+    icon: free_formatter,
+    title: "Free Formatter",
+    subtitle: "Online tool",
+    description: "Website offering free online tools for developers, including formatters, validators, minifiers, escapers, encoders, decoders, digesters, and web resources.",
+    link: "#",
+  },
+  {
+    _id: "8",
+    category: "Web Dev",
+    icon: google_analytics,
+    title: "Google Analytics",
+    subtitle: "Performance",
+    description: "Google tool that tracks and reports website traffic, user behavior, and performance metrics to help analyze and optimize online presence.",
+    link: "#",
+  },
+  {
+    _id: "9",
+    category: "Web Dev",
+    icon: hostinger,
+    title: "Hostinger",
+    subtitle: "Hosting",
+    description: "The best web hosting provider offering affordable hosting services, including shared hosting, VPS, cloud hosting, and domain registration.",
+    link: "#",
+  },
+  {
+    _id: "10",
+    category: "Figma Plugin",
+    icon: iconify,
+    title: "Iconify",
+    subtitle: "Icons",
+    description: "All popular icon sets, one framework. Over 200,000 open source vector icons.",
+    link: "#",
+  },
+  {
+    _id: "11",
+    category: "Figma Plugin",
+    icon: mockupplugin,
+    title: "Mockup Plugin (Figma)",
+    subtitle: "Mockups",
+    description: "Library of high-quality, finely-detailed device and branding mockups for all your projects.",
+    link: "#",
+  },
+  {
+    _id: "12",
+    category: "Design/Media",
+    icon: google_analytics,
+    title: "Muzli",
+    subtitle: "Design inspiration",
+    description: "Muzli is a new-tab browser extension that instantly delivers relevant design stories and inspiration to keep you in the loop.",
+    link: "#",
+  },
+  {
+    _id: "13",
+    category: "Web Dev",
+    icon: google_page_speed,
+    title: "PageSpeed Insights",
+    subtitle: "Performance",
+    description: "Google tool that analyzes web page performance and suggests improvements for faster loading.",
+    link: "#",
+  },
+  {
+    _id: "14",
+    category: "Design/Media",
+    icon: pexels,
+    title: "Pexels",
+    subtitle: "Free stock photos & videos",
+    description: "Free stock photos & videos you can use everywhere. Browse millions of high-quality royalty free stock images & copyright free pictures.",
+    link: "#",
+  },
+  {
+    _id: "15",
+    category: "Design/Media",
+    icon: pixabay,
+    title: "Pixabay",
+    subtitle: "Free stock photos, videos and more",
+    description: "Over 4.9 million+ high quality stock images, videos and music shared by our talented community.",
+    link: "#",
+  },
+  {
+    _id: "16",
+    category: "Code Extension",
+    icon: prettier,
+    title: "Prettier",
+    subtitle: "VSCode Plugin",
+    description: "Prettier is a code formatter that automatically formats code to ensure consistency across projects, supporting various languages like JavaScript, CSS, and HTML.",
+    link: "#",
+  },
+  {
+    _id: "17",
+    category: "Figma Plugin",
+    icon: uicolors,
+    title: "UI Colors",
+    subtitle: "Color palette",
+    description: "A comprehensive collection of UI color palettes, providing developers and designers with curated color schemes for creating visually appealing interfaces.",
+    link: "#",
+  },
+  {
+    _id: "18",
+    category: "Design/Media",
+    icon: uigradients,
+    title: "uiGradients",
+    subtitle: "Color palette",
+    description: "A handpicked collection of beautiful color gradients for designers and developers.",
+    link: "#",
+  },
+  {
+    _id: "19",
+    category: "Design/Media",
+    icon: unsplash,
+    title: "Unsplash",
+    subtitle: "Free stock photos & videos",
+    description: "Beautiful, free images and photos that you can download and use for any project. Better than any royalty free or stock photos.",
+    link: "#",
+  },
+  {
+    _id: "20",
+    category: "Web Dev",
+    icon: vercel,
+    title: "Vercel",
+    subtitle: "Hosting",
+    description: "Cloud platform for deploying and hosting web applications, offering features like automatic scaling, serverless functions, and fast global CDN delivery.",
+    link: "#",
+  },
+];
+
+// export const Portfolio = [
+//   {
+//     _id: "1",
+//     category: "Web",
+//     title: "Desenvolvimento Front-end",
+//     description: "Componentes reutilizáveis e interfaces responsivas com as melhores práticas modernas.",
+//     link: "https://www.youtube.com",
+//   },
+//   {
+//     _id: "2",
+//     category: "Design",
+//     title: "UI/UX Design",
+//     description: "Design systems completos e protótipos interativos para validação de conceitos.",
+//     link: "https://www.youtube.com",
+//   },
+//   {
+//     _id: "3",
+//     category: "SEO",
+//     title: "Otimização SEO",
+//     description: "Estratégias de keyword research e technical SEO para melhor rankeamento.",
+//     link: "https://www.youtube.com",
+//   },
+//   {
+//     _id: "4",
+//     category: "Web",
+//     title: "Back-end Moderno",
+//     description: "APIs performáticas com arquitetura limpa e documentação completa.",
+//     link: "https://www.youtube.com",
+//   },
+//   {
+//     _id: "5",
+//     category: "Design",
+//     title: "Motion Design",
+//     description: "Animções UI e microinterações para melhor engajamento do usuário.",
+//     link: "https://www.youtube.com",
+//   },
+//   {
+//     _id: "6",
+//     category: "SEO",
+//     title: "Análise de Dados",
+//     description: "Relatórios personalizados e dashboards para tomada de decisão.",
+//     link: "https://www.youtube.com",
+//   },
+// ];
